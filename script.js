e6970e7590350f2997e17786448df8e0eb297766
Muhammad Ficky Zulfikar <122990074+Fickygo@users.noreply.github.com>
2023-09-25 22:05:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading, .album-videos, .album-img, .box-container', {origin: 'top'});
+ScrollReveal().reveal('.btn-boy.btnx, .testimonial-box, .maps', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .home-content p, .box-container .box .content p, .box-container .box .content .price, .box-container .box img, .blog-container, .about-content h3', {origin: 'left'});
+ScrollReveal().reveal('.home-content h3,.about-img, .about-content p, .home-sci a, .box-container .box .content h3, .box-container .box .content .stars i', {origin: 'right'});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
