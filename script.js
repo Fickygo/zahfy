f4e26d5acdb9f278f97dde152427c56dcd25b9f5
Muhammad Ficky Zulfikar <122990074+Fickygo@users.noreply.github.com>
2023-10-04 18:21:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,18 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading, .album-videos, .album-img, .box-container', {origin: 'top'});
+ScrollReveal().reveal('.btn-boy.btnx, .testimonial-box, .maps', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .home-content p, .box-container .box .content p, .box-container .box .content .price, .box-container .box img, .blog-container, .about-content h3', {origin: 'left'});
+ScrollReveal().reveal('.home-content h3,.about-img, .about-content p, .home-sci a, .box-container .box .content h3, .box-container .box .content .stars i', {origin: 'right'});
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 50,
